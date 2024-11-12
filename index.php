8c3d4dc8b76e6557f56e6a42a132ae28903ab64f
Mirisaurio <Mirisaurio>
2024-11-12 17:44:28 -0600

    style en formulario

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <header class="contenedor">
+
+    <section class="formulario">
+        <h1>Registrar Usuario:</h1>
     <form id="userForm" method="POST" action="guardar_usuario.php">
-        <label for="first_name">Nombre:</label>
-        <input type="text" id="first_name" name="first_name" required>
-    
-        <label for="last_name">Apellidos:</label>
-        <input type="text" id="last_name" name="last_name" required>
-    
-        <label for="age">Edad:</label>
-        <input type="number" id="age" name="age" required>
-    
-        <label for="curp">CURP:</label>
-        <input type="text" id="curp" name="curp" required>
+        <input type="text" id="first_name" name="first_name" placeholder="Nombre" class="inputFormRegister" required>
+        <br>
+        <input type="text" id="last_name" name="last_name" placeholder="Apellido" class="inputFormRegister" required>
+    <br>
+        <input type="number" id="age" name="age" placeholder="Edad" class="inputFormRegister" required>
+    <br>
+        <input type="text" id="curp" name="curp" placeholder="CURP" class="inputFormRegister" required>
         <input type="hidden" id="user_id">
-    
-        <button type="submit">Guardar</button>
+    <br>
+        <button type="submit" class="saveButton"
+        >Guardar</button>
     </form>
+    </section>
+  
 
-
-
+<section class="tabla">
   <!-- Tabla con registros -->
   <h1>Lista de Usuarios</h1>
     <table border="1">
@@ -43,8 +45,8 @@
         </thead>
         <tbody id="usuarios"></tbody>
     </table>
-
-    
+    </section>
+    </header>
     <script src="script.js"></script>
 </body>
 </html>
